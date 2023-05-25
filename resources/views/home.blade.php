2b7a4ec5dc0d6b907595dd4f88ea3cd10392efaf
Issa <issa.diallo@lemonlearning.fr>
2023-05-25 11:58:22 +0200

    :wrench: Modify the words French in English fix #4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,18 @@
 @section('title', 'Accueil')
 
 @section('content')
+<div class="bg-light p-5 mb-5 text-center">
+    <div class="container">
+        <h1>Agency Pierre & Jean</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+    </div>
+</div>
+
 <div class="container mt-4">
-    <h2>Nos derniers biens</h2>
+    <h2>Our latest properties</h2>
     <div class="row">
         @foreach ($properties as $property )
             <div class="col">
